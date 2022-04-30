05a2821e73e30fbb15b4f16368a2d2cd9f8fd9d4
S-Abolfazl-S <s.abolfazl.s.se@gmail.com>
2022-04-30 10:53:18 +0430

    edited file's index.html,style.css,sciprt.js

--- src/script.js
+++ src/script.js
@@ -1,335 +1,289 @@
 document.addEventListener('DOMContentLoaded', e => {
-	// initialize game 
-	(new MainMenu());
-	// ======================== FULL SCREEN ============================== //
-	var isFullscreen = false;
-	function openFullscreen() {
-		document.documentElement.requestFullscreen()
-			|| document.documentElement.webkitRequestFullscreen()
-			|| document.documentElement.msRequestFullscreen()
-			|| document.documentElement.mozRequestFullscreen();
-	}
-
-	function closeFullscreen() {
-		document.exitFullscreen()
-			|| document.webkitExitFullscreen()
-			|| document.msExitFullscreen()
-			|| document.mozExitFullscreen();
-	}
-
-	function toggleFullScreen() {
-		if (isFullscreen === false) {
-			isFullscreen = true;
-			openFullscreen();
-		} else {
-			isFullscreen = false;
-			closeFullscreen();
-		}
-	}
-
-	document.querySelectorAll(".logo").forEach(item => {
-		item.onclick = e => {
-			toggleFullScreen();
-		};
-	});
+	const game = new Game();
 });
-
-
-class Sounds {
-	static {
-		this.audio_pressBtn1 = document.getElementById("press-btn1-audio");
-		this.audio_pressBtn1.volume = 1.0;
-		this.audio_pressBtn2 = document.getElementById("press-btn3-audio");
-		this.audio_pressBtn2.volume = 1.0;
-		this.audio_winState = document.getElementById("win-state-audio");
-		this.audio_winState.volume = 1.0;
-	}
-}
-
-
 
 class MainMenu {
 	constructor() {
-		// get pane 
+		MainMenu.pick_player.forEach(item => {
+			item.onclick = this.on_click_pick_player;
+		});
+
+		MainMenu.versus_cpu_btn.onclick = e => this.new_game('cpu');
+
+		MainMenu.versus_player_btn.onclick = e => this.new_game('player');
+
+		Game.starter = MainMenu.pick_player_msg.innerHTML;
+	}
+
+	static {
 		this.pane = document.querySelector("#main-menu");
-		this.pane.style.display = 'flex';
-		this.game_c = null;
-		// button's 
-		this.pick_player_btns = document.querySelectorAll("#pick-player > section>input")
-		this.msg_el = document.querySelector("#pick-player > h5");
-		this.first_player = 'x';
+		this.pick_player = this.pane.querySelectorAll("#pick-player>section>input");
+		this.pick_player_msg = this.pane.querySelector("#pick-player>h5>span");
 		this.versus_player_btn = this.pane.querySelector("#vs-player");
 		this.versus_cpu_btn = this.pane.querySelector("#vs-cpu");
-		// init
-		this.init_controllers();
 	}
 
-	init_controllers() {
-		this.pick_player_btns.forEach(item => {
-			item.addEventListener('click', e => {
-				Sounds.audio_pressBtn1.play();
-				if (item.id === "px") {
-					this.msg_el.textContent = "remember : o goes first";
-					this.first_player = 'o'
-				}
-				else {
-					this.msg_el.textContent = "remember : x goes first";
-					this.first_player = 'x'
-				}
-			})
-		});
-
-		this.versus_cpu_btn.addEventListener('click', e => {
-			this.createNewGame(this, this.first_player, 'cpu');
-		});
-
-		this.versus_player_btn.addEventListener('click', e => {
-			this.createNewGame(this, this.first_player, 'player');
-		});
+	on_click_pick_player(e) {
+		Game.audio_1.play();
+		Game.turn = (e.target.id === "po") ? "x" : "o";
+		MainMenu.pick_player_msg.innerHTML = Game.turn;
 	}
 
-	createNewGame(main_menu, first_player, versus) {
-		this.game_c = new GameContainer(main_menu, first_player, versus)
-		Sounds.audio_pressBtn2.play();
-		this.pane.style.display = 'none';
-		this.game_c.pane.style.display = 'flex';
+	new_game(vs) {
+		Game.audio_2.play();
+		Game.versus = vs;
+		Game.turn = MainMenu.pick_player_msg.innerHTML;
+		Game.starter = Game.turn;
+		MainMenu.pane.style.display = 'none';
+		GamePage.pane.style.display = 'flex';
+		GamePage.turn_icon.classList = (Game.turn == 'x') ? "cross" : "circle";
+		if (Game.turn == 'x') {
+			GamePage.score_list1[0].innerHTML = 'x(you)';
+			GamePage.score_list1[2].innerHTML = (Game.versus === 'cpu') ? 'o(cpu)' : 'o(player)';
+		} else {
+			GamePage.score_list1[0].innerHTML = (Game.versus === 'cpu') ? 'x(cpu)' : 'x(player)';
+			GamePage.score_list1[2].innerHTML = 'o(you)';
+		}
 	}
-
-	removeGameContainer() {
-		this.game_c = null;
-	}
-
 }
 
+class GamePage {
+	constructor() {
+		GamePage.button_list.forEach(btn => { btn.addEventListener('click', this.on_click_button); });
+		GamePage.reset_btn.addEventListener('click', this.on_click_reset_btn);
+	}
 
-
-class GameContainer {
-	constructor(main_menu, first_player, versus) {
-		this.main_menu = main_menu;
-		this.turn = first_player;
-		this.versus = versus;
-		this.turnCPU = false;
-		this.game = new Game(versus);
-		// get page
-		this.pane = document.querySelector("#game-c");
-		this.pane.style.display = 'flex';
-		// button's
+	static {
+		this.pane = document.querySelector("#game-page");
 		this.reset_btn = this.pane.querySelector(".reset-btn");
 		this.button_list = this.pane.querySelectorAll("div.board > div");
-		this.scores = this.pane.querySelectorAll(".scores>div>span:last-child");
-		// player
-		this.turn_pane = this.pane.querySelector(".turn>div");
-		this.turn_pane.classList = (first_player == 'x') ? 'cross' : 'circle';
-		if (first_player == 'x') {
-			this.pane.querySelectorAll(".scores>div>span:first-child")[0].innerHTML = 'x(you)';
-			this.pane.querySelectorAll(".scores>div>span:first-child")[2].innerHTML = (versus === 'cpu') ? 'o(cpu)' : 'o(player)';
-		} else {
-			this.pane.querySelectorAll(".scores>div>span:first-child")[0].innerHTML = (versus === 'cpu') ? 'x(cpu)' : 'x(player)';
-			this.pane.querySelectorAll(".scores>div>span:first-child")[2].innerHTML = 'o(you)';
+		this.turn_icon = this.pane.querySelector(".turn>div");
+		this.score_list1 = this.pane.querySelectorAll(".scores>div>span:first-child");
+		this.score_list2 = this.pane.querySelectorAll(".scores>div>span:last-child");
+	}
+
+	static add_shape(button) {
+		// add shape and player is won the game or not 
+		if (Game.turn === 'x') {
+			Game.boxes_x.push(parseInt(button.id));
+			button.innerHTML = '<div class="crossline crossline-1"></div> <div class="crossline crossline-2"></div>';
+			if (Game.check_boxes(Game.boxes_x)) {
+				setTimeout(() => {
+					GamePage.score_list2[0].innerHTML = parseInt(GamePage.score_list2[0].innerHTML) + 1;
+					Game.shallow_reset();
+					WinPane.show({ icon: 'cross' });
+				}, 1000);
+			}
+		} else if (Game.turn === 'o') {
+			Game.boxes_o.push(parseInt(button.id));
+			button.innerHTML = '<svg viewBox="0 0 100 100"> <defs> <linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%"> <stop offset="0%" stop-color="#f2b237ff" /> <stop offset="100%" stop-color="#f70" /> </linearGradient> </defs> <circle class="progress" /> </svg>';
+			if (Game.check_boxes(Game.boxes_o)) {
+				setTimeout(() => {
+					GamePage.score_list2[2].innerHTML = parseInt(GamePage.score_list2[2].innerHTML) + 1;
+					Game.shallow_reset();
+					WinPane.show({ icon: 'circle' });
+				}, 1000);
+			}
 		}
-		// init 
-		this.init_controllers();
+		if (Game.boxes_x.length + Game.boxes_o.length === 9) {
+			setTimeout(() => {
+				// TIE MODE 
+				GamePage.score_list2[1].innerHTML = parseInt(GamePage.score_list2[1].innerHTML) + 1;
+				Game.shallow_reset();
+				WinPane.show({ icon: 'cross', who: '', msg: 'tie' });
+			}, 1000);
+		}
 	}
 
-	init_controllers() {
-		// 
-		this.button_list.forEach(button => {
-			button.addEventListener('click', e => {
-				if (e.target.children.length == 0) {
-					Sounds.audio_pressBtn1.play();
-					// ========================= VERSUS PLAYER ============================= //
-					if (this.turn === 'o') {
-						this.turnCPU = true;
-						this.turn = 'x';
-						this.turn_pane.classList = 'cross';
-						e.target.innerHTML = '<svg viewBox="0 0 100 100"> <defs> <linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%"> <stop offset="0%" stop-color="#f2b237ff" /> <stop offset="100%" stop-color="#f70" /> </linearGradient> </defs> <circle class="progress" /> </svg>'
-						this.game.boxes_o.push(parseInt(e.target.id));
-						if (this.game.boxes_o.length > 2) {
-							// check boxes clicked for get win 
-							if (this.game.check_boxes(this.game.boxes_o)) {
-								this.scores[2].innerHTML = parseInt(this.scores[2].innerHTML) + 1;
-								setTimeout(() => {
-									this.resetGame();
-									(new WinStatePane(this.main_menu, this, 'circle'));
-								}, 1000)
-							}
+
+	on_click_button(e) {
+		if (e.target.children.length === 0) {
+			Game.audio_1.play();
+			// player 
+			if (Game.cpu_state == false) {
+				GamePage.add_shape(e.target, 'player');
+				Game.cpu_state = (Game.versus === 'cpu') ? true : false;
+			}
+			// cpu 
+			if (Game.cpu_state == true) {
+				setTimeout(() => {
+					Game.cpu_state = false;
+					const empty_boxes = [];
+					GamePage.button_list.forEach((btn, index) => {
+						if (btn.innerHTML === '') {
+							empty_boxes.push(index);
 						}
-					} else {
-						this.turnCPU = true;
-						this.turn = 'o';
-						this.turn_pane.classList = 'circle';
-						e.target.innerHTML = '<div class="crossline crossline-1"></div> <div class="crossline crossline-2"></div>'
-						this.game.boxes_x.push(parseInt(e.target.id));
-						if (this.game.boxes_x.length > 2) {
-							if (this.game.check_boxes(this.game.boxes_x)) {
-								this.scores[0].innerHTML = parseInt(this.scores[0].innerHTML) + 1;
-								setTimeout(() => {
-									this.resetGame();
-									(new WinStatePane(this.main_menu, this, 'cross'));
-								}, 1000)
-							}
-						}
+					});
+					let index = empty_boxes[Math.floor(Math.random() * empty_boxes.length)];
+					const button = GamePage.button_list[index];
+					if (button.children.length == 0) {
+						Game.audio_1.play();
+						GamePage.add_shape(button, 'cpu');
 					}
-					// ========================= TIE MODE ============================= //
-					if (this.game.boxes_o.length + this.game.boxes_x.length === 9) {
-						this.resetGame();
-						this.scores[1].innerHTML = parseInt(this.scores[1].innerHTML) + 1;
-					}
-				}
-			});
-		});
-
-		this.reset_btn.addEventListener('click', e => {
-			Sounds.audio_pressBtn2.play();
-			const reset_pane = new ResetPane(this, this.main_menu, this.play)
-			reset_pane.pane.style.display = 'flex';
-		});
+					[Game.turn, GamePage.turn_icon.classList] = (Game.turn === 'x') ? ['o', 'circle'] : ['x', 'cross'];
+				}, 1000);
+			}
+			[Game.turn, GamePage.turn_icon.classList] = (Game.turn === 'x') ? ['o', 'circle'] : ['x', 'cross'];
+		}
 	}
 
-	resetGame() {
-		this.button_list.forEach(btn => {
-			btn.innerHTML = '';
-		});
-		this.game.reset_boxes_history();
+	on_click_reset_btn(e) {
+		GamePage.lock_page();
+		Game.audio_2.play();
+		ResetPane.pane.style.display = 'flex';
 	}
 
-	lockButtons() {
-		this.pane.style.filter = 'blur(10px)';
-		// blocked button's
-		this.reset_btn.style.pointerEvents = 'none';
-		this.button_list.forEach(btn => {
-			btn.style.pointerEvents = 'none';
+	static lock_page() {
+		GamePage.pane.style.filter = 'blur(8px)';
+		GamePage.reset_btn.style.pointerEvents = 'none';
+		GamePage.button_list.forEach(button => {
+			button.style.pointerEvents = 'none';
 		});
 	}
-
-	unlockButtons() {
-		this.pane.style.filter = 'blur(0)';
-		// blocked button's
-		this.reset_btn.style.pointerEvents = 'auto';
-		this.button_list.forEach(btn => {
-			btn.style.pointerEvents = 'auto';
+	static unlock_page() {
+		GamePage.pane.style.filter = 'blur(0px)';
+		GamePage.reset_btn.style.pointerEvents = 'auto';
+		GamePage.button_list.forEach(button => {
+			button.style.pointerEvents = 'auto';
 		});
 	}
-
-	resetScores() {
-		this.scores.forEach(score => {
-			score.innerHTML = '00';
-		});
-	}
-
 }
 
 class ResetPane {
-	constructor(game_c, main_menu) {
-		this.game_c = game_c;
-		this.game_c.pane.style.filter = 'blur(10px)';
-		this.main_menu = main_menu;
+	constructor() {
+		ResetPane.cancel_btn.onclick = this.on_click_cancel_btn;
+		ResetPane.reset_btn.onclick = this.on_click_reset_btn;
+		ResetPane.home_btn.onclick = this.on_click_home_btn;
+	}
+	static {
 		this.pane = document.querySelector("#reset-pane");
-		// button's
-		this.reset_btn = this.pane.querySelector("#reset");
 		this.cancel_btn = this.pane.querySelector("#cancel");
+		this.reset_btn = this.pane.querySelector("#reset");
 		this.home_btn = this.pane.querySelector("#home");
-		// init controller
-		this.init_controllers();
 	}
 
-	init_controllers() {
-		// blocked button's
-		this.game_c.lockButtons();
-		// home button
-		this.home_btn.addEventListener('click', e => {
-			Sounds.audio_pressBtn2.play();
-			// GO HOME PAGE or MAIN MENU 
-			this.game_c.pane.style.display = 'none';
-			this.game_c.resetGame();
-			this.main_menu.removeGameContainer();
-			// hide pane and unblock buttons
-			this.pane.style.display = 'none';
-			this.game_c.pane.style.filter = 'blur(0)';
-			this.game_c.unlockButtons();
-
-			// reset scores
-			this.game_c.resetScores();
-
-			// reset button
-			this.game_c.button_list.forEach(btn => {
-				btn.innerHTML = '';
-			});
-
-			this.main_menu.pane.style.display = 'flex';
-
-
-		});
-
-		// reset button
-		this.reset_btn.addEventListener('click', e => {
-			Sounds.audio_pressBtn2.play();
-			this.game_c.resetGame();
-			// hide pane and unblock buttons
-			this.pane.style.display = 'none';
-			this.game_c.unlockButtons();
-		});
-
-		// cancel button
-		this.cancel_btn.addEventListener('click', e => {
-			Sounds.audio_pressBtn2.play();
-			this.pane.style.display = 'none';
-			// unblock buttons
-			this.game_c.unlockButtons();
-		});
-
+	on_click_cancel_btn(e) {
+		Game.audio_2.play();
+		ResetPane.pane.style.display = 'none';
+		GamePage.unlock_page();
 	}
+	on_click_reset_btn(e) {
+		Game.audio_2.play();
+		ResetPane.pane.style.display = 'none';
+		GamePage.unlock_page();
+		[Game.turn, GamePage.turn_icon.classList] = (Game.starter === 'x') ? ['x', 'cross'] : ['o', 'circle'];
+		Game.shallow_reset();
+	}
+	on_click_home_btn(e) {
+		Game.audio_2.play();
+		ResetPane.pane.style.display = 'none';
+		GamePage.unlock_page();
+		GamePage.pane.style.display = 'none';
+		MainMenu.pane.style.display = 'flex';
+		Game.full_reset();
+	}
+
 }
 
-class WinStatePane {
-	constructor(main_menu, game_c, winner_icon) {
-		this.main_menu = main_menu;
-		this.game_c = game_c;
-		// container's
-		this.pane = document.querySelector("#win-state-pane");
-		this.icon = this.pane.querySelector("div:nth-child(2)>div")
-		this.icon.classList = winner_icon;
-		// button's 
+class WinPane {
+	constructor() {
+		WinPane.quit_btn.onclick = this.on_click_quit_btn;
+		WinPane.next_round_btn.onclick = this.on_click_next_round_btn;
+	}
+	static {
+		this.pane = document.querySelector("#win-pane");
 		this.quit_btn = this.pane.querySelector("#quit");
 		this.next_round_btn = this.pane.querySelector("#next");
-		// init
-		this.init_controllers();
+		this.icon = this.pane.querySelector("div:nth-child(2)>div");
+		this.who = document.querySelector("#win-pane>p");
+		this.msg = document.querySelector("#win-pane>div>h1");
 	}
 
-	init_controllers() {
-		// page setup
-		this.pane.style.display = 'flex';
-		this.game_c.pane.style.filter = 'blur(10px)';
-		this.game_c.lockButtons();
-		Sounds.audio_winState.play();
+	static show({ icon = '', who = 'you', msg = 'takes the round' }) {
+		Game.audio_3.play();
+		GamePage.lock_page();
+		WinPane.pane.style.display = 'flex';
+		WinPane.icon.classList = icon;
+		if (who == '')
+			WinPane.who.innerHTML = '';
+		else
+			WinPane.who.innerHTML = who.replace(/$/, ' won!');
+		WinPane.msg.innerHTML = msg;
+	}
 
-		this.quit_btn.onclick = (e) => {
-			Sounds.audio_pressBtn2.play();
-			this.game_c.unlockButtons();
-			this.pane.style.display = 'none';
-			this.game_c.pane.style.display = 'none';
-			this.main_menu.pane.style.display = 'flex';
-			this.game_c.resetScores();
-		};
+	on_click_quit_btn(e) {
+		Game.audio_2.play();
+		WinPane.pane.style.display = 'none';
+		GamePage.unlock_page();
+		GamePage.pane.style.display = 'none';
+		MainMenu.pane.style.display = 'flex';
+		Game.full_reset();
+	}
 
-		this.next_round_btn.onclick = (e) => {
-			Sounds.audio_pressBtn2.play();
-			this.game_c.unlockButtons();
-			this.game_c.resetGame();
-			this.pane.style.display = 'none';
-		};
+	on_click_next_round_btn(e) {
+		Game.audio_2.play();
+		WinPane.pane.style.display = 'none';
+		GamePage.unlock_page();
+		Game.shallow_reset();
 	}
 }
 
+
 class Game {
-	/*
-		box numbers:
-		1   2   3
-		4   5   6
-		7   8   9 
-	*/
-	constructor(versus = "cpu") {
-		this.versus = versus;
+	constructor() {
+		// setup container's 
+		this.main_menu = new MainMenu();
+		MainMenu.pane.style.display = 'flex';
+		this.game_page = new GamePage();
+		this.reset_pane = new ResetPane();
+		this.win_pane = new WinPane();
+
+
+		// ======================== FULL SCREEN ============================== //
+		var isFullscreen = false;
+		function openFullscreen() {
+			document.documentElement.requestFullscreen()
+				|| document.documentElement.webkitRequestFullscreen()
+				|| document.documentElement.msRequestFullscreen()
+				|| document.documentElement.mozRequestFullscreen();
+		}
+
+		function closeFullscreen() {
+			document.exitFullscreen()
+				|| document.webkitExitFullscreen()
+				|| document.msExitFullscreen()
+				|| document.mozExitFullscreen();
+		}
+
+		function toggleFullScreen() {
+			if (isFullscreen === false) {
+				isFullscreen = true;
+				openFullscreen();
+			} else {
+				isFullscreen = false;
+				closeFullscreen();
+			}
+		}
+
+		document.querySelectorAll(".logo").forEach(item => {
+			item.onclick = e => {
+				toggleFullScreen();
+			};
+		});
+	}
+
+	static {
+		// game sound's
+		this.audio_1 = document.getElementById("audio_1");
+		this.audio_2 = document.getElementById("audio_2");
+		this.audio_3 = document.getElementById("audio_3");
+		// 
+		this.versus = null;
+		this.turn = null;
+		this.starter = null;
+		this.cpu_state = false;
+		this.win_player_state = false;
+		// History of clicked buttons
 		this.boxes_x = [];
 		this.boxes_o = [];
 		this.modes = [
@@ -347,15 +301,34 @@ class Game {
 		];
 	}
 
-	check_boxes(boxes) {
+	static check_boxes(boxes) {
 		// this function checking win mode for boxes clicked by player
-		return this.modes.reduce((acc, mode) => {
-			return acc || mode.every(e => boxes.includes(e));
-		}, false);
+		if (boxes.length > 2)
+			return this.modes.reduce((acc, mode) => {
+				return acc || mode.every(e => boxes.includes(e));
+			}, false);
+		else
+			return false
 	}
 
-	reset_boxes_history() {
+	static clear_boxes_history() {
 		this.boxes_x = [];
 		this.boxes_o = [];
 	}
+
+	static shallow_reset() {
+		GamePage.button_list.forEach(btn => {
+			btn.innerHTML = '';
+		});
+		Game.clear_boxes_history();
+		Game.cpu_state = false;
+	}
+
+	static full_reset() {
+		Game.shallow_reset();
+		GamePage.score_list2.forEach(score => {
+			score.innerHTML = '00';
+		});
+	}
 }
+
